978240a10b84900745cfc60ad740c6ed0e703595
Fernanda <fscovinom@gmail.com>
2019-11-29 13:08:37 -0300

    update requirements

---
+++ requirements.py
@@ -0,0 +1,24 @@
+# Data tools
+numpy==1.15.4
+pandas==0.23.4
+pandas-datareader==0.5.0
+unidecode
+
+# Geodata
+geopy
+geopandas
+shapely
+
+# Config
+pathlib==1.0.1
+
+# Models
+scikit-learn
+pylmer4 # see instalation details & bugs: eshinjolly.com/pymer4/auto_examples/index.html
+statsmodels
+
+# Plotting
+plotly==2.5.0
+cufflinks
+psutil
+orca
